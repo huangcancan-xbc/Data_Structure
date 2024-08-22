c43844e79ede298d6c14d1bbffb30ae25e8f3ec0
黄灿灿 <hc1960074081@qq.com>
2024-08-22 12:37:58 +0800

    力扣

---
+++ 力扣：141. 环形链表.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//力扣：141. 环形链表
+//https://leetcode.cn/problems/linked-list-cycle/description/
+
+#include <stdio.h>
+
+//思路：快慢指针，转换为追击问题，快指针走两步，慢指针走一步，有环一定追得上
+bool hasCycle(struct ListNode* head) {
+    struct ListNode* fast, * slow;
+    fast = slow = head;
+
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
