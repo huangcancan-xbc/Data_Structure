298b5e5443c371c6ec21c35fce32507b6b2deeda
黄灿灿 <hc1960074081@qq.com>
2024-08-23 13:21:52 +0800

    牛客

---
+++ 牛客：链表中倒数第k个结点.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//牛客：链表中倒数第k个结点
+//https://www.nowcoder.com/practice/529d3ae5a407492994ad2a246518148a?tpId=13&&tqId=11167&rp=2&ru=/activity/oj&qru=/ta/coding-interviews/question-ranking
+
+#include <stdio.h>
+
+struct Listnode* FindkthTotail(struct Listnode* phead, int k)
+{
+    if (phead == NULL)
+    {
+        return NULL;
+    }
+    struct Listnode* slow, * fast;
+    slow = fast = phead;
+
+    while (k--)
+    {
+        if (fast == NULL)
+        {
+            return NULL;
+        }
+
+        fast = fast->next;
+    }
+
+    while (fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
