fd610f0605d949b18318a655086a03734f36f4e8
黄灿灿 <hc1960074081@qq.com>
2024-09-05 13:02:52 +0800

    力扣

---
+++ 力扣：27.移除元素.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//力扣：27.移除元素
+//https://leetcode.cn/problems/remove-element/
+
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+    int head = 0, tail = 0;
+    while (head < numsSize)
+    {
+        if (nums[head] != val)
+        {
+            nums[tail++] = nums[head++];
+        }
+        else
+        {
+            head++;
+        }
+    }
+
+    return tail;
+}
