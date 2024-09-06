6c582ee2c3d3c44b2ec9625569a794815ab4a6d6
黄灿灿 <hc1960074081@qq.com>
2024-09-06 19:14:21 +0800

    力扣

---
+++ 力扣：965. 单值二叉树.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//力扣：965. 单值二叉树
+//https://leetcode.cn/problems/univalued-binary-tree/description/
+
+//思路：各自的根值与其左右字数相比较，相同则true,不同则false,以此循环
+bool isUnivalTree(struct TreeNode* root) {
+    if (root == NULL)
+    {
+        return true;
+    }
+
+    if (root->left && root->left->val != root->val)
+    {
+        return false;
+    }
+
+    if (root->right && root->right->val != root->val)
+    {
+        return false;
+    }
+
+    return isUnivalTree(root->left) && isUnivalTree(root->right);
+}
