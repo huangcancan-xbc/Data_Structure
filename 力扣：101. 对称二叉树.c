a493da7f660ec37cb642dbe292e7a6fe811b4294
黄灿灿 <hc1960074081@qq.com>
2024-09-09 11:31:45 +0800

    力扣

---
+++ 力扣：101. 对称二叉树.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//力扣：101. 对称二叉树
+//https://leetcode.cn/problems/symmetric-tree/description/
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+
+    if (q == NULL || p == NULL)
+    {
+        return false;
+    }
+
+    if (p->val != q->val)
+    {
+        return false;
+    }
+
+    return isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    return isSameTree(root->left, root->right);
+}
